330fda2ea312c1b09f4f258b5b115d758b53a31c
Garrett Birkel <gbirkel@me.com>
2023-01-24 20:29:39 -0800

    Build now succeeds but introspection test still fails. Some TODO notes.

--- src/FSharp.Data.GraphQL.Shared/JSON.fs
+++ src/FSharp.Data.GraphQL.Shared/JSON.fs
@@ -23,6 +23,13 @@ let configureSerializerOptions (additionalConverters: JsonConverter seq) (option
     options
 
 let getSerializerOptions (additionalConverters: JsonConverter seq) =
+    // TODO GBirkel: Needed to make Tests.IntrospectionTests.Introspection pass, but makes other tests fail?
+    // https://github.com/Tarmil/FSharp.SystemTextJson/blob/master/docs/Customizing.md#skippable-option-fields
+    //let options =
+    //        JsonFSharpOptions.Default()
+    //            .WithSkippableOptionFields()
+    //            .ToJsonSerializerOptions()
+    //configureSerializerOptions additionalConverters options
     JsonSerializerOptions () |> configureSerializerOptions additionalConverters
 
 let public serializerOptions = getSerializerOptions Seq.empty
