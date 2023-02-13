785a04968075f946b58c24a2c74dce385e8683e6
Garrett Birkel <gbirkel@me.com>
2023-02-13 15:07:19 -0800

    Update samples/star-wars-api/HttpHandlers.fs
    Co-authored-by: Andrii Chebukin <xperiandri@live.ru>

--- samples/star-wars-api/HttpHandlers.fs
+++ samples/star-wars-api/HttpHandlers.fs
@@ -230,7 +230,7 @@ module HttpHandlers =
                 let operationName = request.OperationName |> Skippable.toOption
 
                 operationName
-                |> Option.iter (fun on -> logger.LogTrace ($"GraphQL operation name: {{on}}", on))
+                |> Option.iter (fun on -> logger.LogTrace ($"GraphQL operation name: {{operationName}}", on))
 
                 let variables = request.Variables |> Skippable.toOption
 
