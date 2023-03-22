862f10c7bbf9f19dd9e2483a0bfe0585890b6eb1
Garrett Birkel <gbirkel@me.com>
2023-03-21 17:20:52 -0700

    Temporarily un-nesting some code prior to taking it apart to move pieces down into Executor.

--- samples/star-wars-api/MultipartRequest.fs
+++ samples/star-wars-api/MultipartRequest.fs
@@ -53,85 +53,89 @@ type MultipartRequest =
 
 /// Contains tools for working with GraphQL multipart requests.
 module MultipartRequest =
+    let findFile (map : IDictionary<string, string>) (files : IDictionary<string, File>) (operationIndex : int option) (operation : GQLRequestContent) (varName : string) (varValue : JsonElement) =
+        let tryPickMultipleFilesFromMap (length : int) (varName : string) =
+            Seq.init length (fun ix ->
+                match map.TryGetValue(sprintf "%s.%i" varName ix) with
+                | (true, v) -> Some v
+                | _ -> None)
+            |> Seq.map (fun key ->
+                key |> Option.map (fun key ->
+                    match files.TryGetValue(key) with
+                    | (true, v) -> Some v
+                    | _ -> None)
+                |> Option.flatten)
+            |> List.ofSeq
+        let pickMultipleFilesFromMap (length : int) (varName : string) =
+            Seq.init length (fun ix -> map.[sprintf "%s.%i" varName ix])
+            |> Seq.map (fun key -> files.[key])
+            |> List.ofSeq
+        let tryPickSingleFileFromMap varName =
+            let found = map |> Seq.choose (fun kvp -> if kvp.Key = varName then Some files.[kvp.Value] else None) |> List.ofSeq
+            match found with
+            | [x] -> Some x
+            | _ -> None
+        let pickSingleFileFromMap varName =
+            map
+            |> Seq.choose (fun kvp -> if kvp.Key = varName then Some files.[kvp.Value] else None)
+            |> Seq.exactlyOne
+        let pickFileRequestFromMap (request : UploadRequest) varName =
+            {   UploadRequest.Single = pickSingleFileFromMap (sprintf "%s.single" varName)
+                Multiple = pickMultipleFilesFromMap request.Multiple.Length (sprintf "%s.multiple" varName)
+                NullableMultiple = request.NullableMultiple |> Option.map (fun x -> pickMultipleFilesFromMap x.Length (sprintf "%s.nullableMultiple" varName))
+                NullableMultipleNullable = request.NullableMultipleNullable |> Option.map (fun x -> tryPickMultipleFilesFromMap x.Length (sprintf "%s.nullableMultipleNullable" varName)) }
+
+        let rec isUpload (t : InputType) =
+            match t with
+            | NamedType tname -> tname = "Upload" || tname = "UploadRequest"
+            | ListType t | NonNullType t -> isUpload t
+
+        let ast = Parser.parse operation.Query
+        let vardefs =
+            ast.Definitions
+            |> List.choose (function OperationDefinition def -> Some def.VariableDefinitions | _ -> None)
+            |> List.collect id
+        let vardef = vardefs |> List.find (fun x -> x.VariableName = varName)
+        if not (isUpload vardef.Type)
+        then varValue
+        else
+            match varValue with
+            | _ when varValue.ValueKind = JsonValueKind.Object ->
+                let request = JsonSerializer.Deserialize<UploadRequest>(varValue)
+                //let request = jreq.Deserialize<UploadRequest>(jsonSerializer)
+                let varName =
+                    match operationIndex with
+                    | Some operationIndex -> sprintf "%i.variables.%s" operationIndex varName
+                    | None -> sprintf "variables.%s" varName
+                pickFileRequestFromMap request varName |> box
+            | _ when varValue.ValueKind = JsonValueKind.Array ->
+                varValue.EnumerateArray()
+                |> Seq.cast<obj>
+                |> Seq.mapi (fun valueIndex _ ->
+                    let varName =
+                        match operationIndex with
+                        | Some operationIndex -> sprintf "%i.variables.%s.%i" operationIndex varName valueIndex
+                        | None -> sprintf "variables.%s.%i" varName valueIndex
+                    tryPickSingleFileFromMap varName |> Option.map box |> Option.toObj)
+                |> box
+            | _ ->
+                let varName =
+                    match operationIndex with
+                    | Some operationIndex -> sprintf "%i.variables.%s" operationIndex varName
+                    | None -> sprintf "variables.%s" varName
+                tryPickSingleFileFromMap varName |> Option.map box |> Option.toObj
+
+    let mapOperation (map : IDictionary<string, string>) (files : IDictionary<string, File>) (operationIndex : int option) (operation : GQLRequestContent) =
+        let varsMaybe = operation.Variables |> Skippable.toOption
+        let varsMaybeWithFiles = varsMaybe |> Option.map (fun vars ->
+                vars |> Seq.map (fun k -> (k.Key, (findFile map files operationIndex operation k.Key k.Value))) |> Map.ofSeq |> ImmutableDictionary.ToImmutableDictionary
+            )
+        { operation with Variables = varsMaybeWithFiles |> Skippable.ofOption }
+
     let private parseOperations (operations: GQLRequestContent list) (map : IDictionary<string, string>) (files : IDictionary<string, File>) : GQLRequestContent list =
-        let mapOperation (operationIndex : int option) (operation : GQLRequestContent) =
-            let findFile (varName : string) (varValue : JsonElement) =
-                let tryPickMultipleFilesFromMap (length : int) (varName : string) =
-                    Seq.init length (fun ix ->
-                        match map.TryGetValue(sprintf "%s.%i" varName ix) with
-                        | (true, v) -> Some v
-                        | _ -> None)
-                    |> Seq.map (fun key ->
-                        key |> Option.map (fun key ->
-                            match files.TryGetValue(key) with
-                            | (true, v) -> Some v
-                            | _ -> None)
-                        |> Option.flatten)
-                    |> List.ofSeq
-                let pickMultipleFilesFromMap (length : int) (varName : string) =
-                    Seq.init length (fun ix -> map.[sprintf "%s.%i" varName ix])
-                    |> Seq.map (fun key -> files.[key])
-                    |> List.ofSeq
-                let tryPickSingleFileFromMap varName =
-                    let found = map |> Seq.choose (fun kvp -> if kvp.Key = varName then Some files.[kvp.Value] else None) |> List.ofSeq
-                    match found with
-                    | [x] -> Some x
-                    | _ -> None
-                let pickSingleFileFromMap varName =
-                    map
-                    |> Seq.choose (fun kvp -> if kvp.Key = varName then Some files.[kvp.Value] else None)
-                    |> Seq.exactlyOne
-                let pickFileRequestFromMap (request : UploadRequest) varName =
-                    { UploadRequest.Single = pickSingleFileFromMap (sprintf "%s.single" varName)
-                      Multiple = pickMultipleFilesFromMap request.Multiple.Length (sprintf "%s.multiple" varName)
-                      NullableMultiple = request.NullableMultiple |> Option.map (fun x -> pickMultipleFilesFromMap x.Length (sprintf "%s.nullableMultiple" varName))
-                      NullableMultipleNullable = request.NullableMultipleNullable |> Option.map (fun x -> tryPickMultipleFilesFromMap x.Length (sprintf "%s.nullableMultipleNullable" varName)) }
-                let rec isUpload (t : InputType) =
-                    match t with
-                    | NamedType tname -> tname = "Upload" || tname = "UploadRequest"
-                    | ListType t | NonNullType t -> isUpload t
-                let ast = Parser.parse operation.Query
-                let vardefs =
-                    ast.Definitions
-                    |> List.choose (function OperationDefinition def -> Some def.VariableDefinitions | _ -> None)
-                    |> List.collect id
-                let vardef = vardefs |> List.find (fun x -> x.VariableName = varName)
-                if not (isUpload vardef.Type)
-                then varValue
-                else
-                    match varValue with
-                    | _ when varValue.ValueKind = JsonValueKind.Object ->
-                        let request = JsonSerializer.Deserialize<UploadRequest>(varValue)
-                        //let request = jreq.Deserialize<UploadRequest>(jsonSerializer)
-                        let varName =
-                            match operationIndex with
-                            | Some operationIndex -> sprintf "%i.variables.%s" operationIndex varName
-                            | None -> sprintf "variables.%s" varName
-                        pickFileRequestFromMap request varName |> box
-                    | _ when varValue.ValueKind = JsonValueKind.Array ->
-                        varValue.EnumerateArray()
-                        |> Seq.cast<obj>
-                        |> Seq.mapi (fun valueIndex _ ->
-                            let varName =
-                                match operationIndex with
-                                | Some operationIndex -> sprintf "%i.variables.%s.%i" operationIndex varName valueIndex
-                                | None -> sprintf "variables.%s.%i" varName valueIndex
-                            tryPickSingleFileFromMap varName |> Option.map box |> Option.toObj)
-                        |> box
-                    | _ ->
-                        let varName =
-                            match operationIndex with
-                            | Some operationIndex -> sprintf "%i.variables.%s" operationIndex varName
-                            | None -> sprintf "variables.%s" varName
-                        tryPickSingleFileFromMap varName |> Option.map box |> Option.toObj
-            let varsMaybe = operation.Variables |> Skippable.toOption
-            let varsMaybeWithFiles = varsMaybe |> Option.map (fun vars ->
-                    vars |> Seq.map (fun k -> (k.Key, (findFile k.Key k.Value))) |> Map.ofSeq |> ImmutableDictionary.ToImmutableDictionary
-                )
-            { operation with Variables = varsMaybeWithFiles |> Skippable.ofOption }
         match operations with
-        | [ operation ] -> [ mapOperation None operation ]
-        | operations -> operations |> List.mapi (fun ix operation -> mapOperation (Some ix) operation)
+        | [ operation ] -> [ mapOperation map files None operation ]
+        | operations -> operations |> List.mapi (fun ix operation -> mapOperation map files (Some ix) operation)
 
     /// Reads a GraphQL multipart request from a MultipartReader.
     let read cancellationToken (reader : MultipartReader) : Threading.Tasks.Task<MultipartRequest> =
